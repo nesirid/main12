1842d628d636d399b185cc1f925d3d29fc12034c
nesirid <nasirid@code.edu.az>
2024-03-05 02:19:52 +0400

    first commit

---
+++ SealedStructNullabeInterface/SealedStructNullabeInterface/Program.cs
@@ -0,0 +1,39 @@
+﻿using SealedStructNullabeInterface;
+
+//book.Name = "dwadwadwa";
+
+//Console.WriteLine(book.Name);
+
+//int num = 5;
+//TestStruct class1 = new TestStruct();
+
+
+//string name = null;
+//int? name = null;
+//Book book = new Book();
+//Console.WriteLine(Book.);
+
+Book book1 = new()
+{
+    Id = 1,
+    Name = "Isgendername" 
+};
+
+Book book2 = new()
+{
+    Id = 1,
+    Name = "Xosrov ve Sirin"
+};
+
+Book[] books = { book1 , book2 };
+void GetBookById(Book[] books, int? id = null)
+{ 
+    if(id == null)
+{
+    Console.WriteLine("Your id is null");
+        return;
+}
+    var result = books.FirstOrDefault(m => m.Id == id);
+}
+
+GetBookById(books);
---
+++ SealedStructNullabeInterface/SealedStructNullabeInterface/TestStruct.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SealedStructNullabeInterface
+{
+    internal class TestStruct
+    {
+        public int Num { get; set; } = 200;
+
+    }
+}
